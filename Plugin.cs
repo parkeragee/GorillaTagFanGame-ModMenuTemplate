56e3b9c3c8c5905417371d0ca1df493d027435ba
Maximility <110491908+Maximility@users.noreply.github.com>
2023-10-08 17:24:13 -0500

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Maximility.Menu;
+using MelonLoader;
+using Photon.Pun;
+using Photon.VR;
+using UnityEngine;
+
+namespace Maximility
+{
+	public class Plugin : MelonMod
+	{
+        #region Main
+
+        public bool menuInitialized;
+
+        public override void OnUpdate()
+		{
+			if (!this.menuInitialized)
+			{
+				Menu.Menu.LoadOnce();
+				this.menuInitialized = true;
+			}
+
+			Menu.Menu.Load();
+
+			new ModManager().Mods();
+
+			if (!PhotonNetwork.IsConnected)
+			{
+				PhotonNetwork.ConnectUsingSettings();
+			}
+		}
+
+		#endregion
+
+		#region Bools
+
+		//Movement Bools
+		public static bool ExcelFly;
+        public static bool TFly;
+
+        #endregion
+    }
+}
